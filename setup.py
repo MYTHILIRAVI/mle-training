a31c9a28517bdc540a08be31d4e556bc3f3a3eaa
mythili ravi <mythili.ravi@tigeranalytics.com>
2022-06-07 14:03:16 +0530

    completed 4.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="housing",
+    name="Housing-MYTHILI",
     version="v0.3",
     author="Mythili Ravi",
     author_email="mythili.ravi@tigeranalytics.com",
